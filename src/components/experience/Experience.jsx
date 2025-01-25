46dce1c9d01f577b4324a0e9b6ed85f23296d8d1
Nestor Rios <neriga10@hotmail.com>
2025-01-24 18:49:11 -0600

    feat: add Enemy model with animations

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,16 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Floor, Lights } from "./enviroment";
-import { ControlPlayer, GunModel } from "./player";
+import { ControlPlayer } from "./player";
+import { Enemy } from "./enemies";
 
 export const Experience = () => {
   return (
     <Canvas shadows>
-      <ControlPlayer />
+      <Enemy position={[0, -1, 0]} />
+      {/* <ControlPlayer /> */}
       <OrbitControls />
       <Lights />
       <Floor />
-      <GunModel />
     </Canvas>
   );
 };
